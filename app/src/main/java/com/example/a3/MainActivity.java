a0913f224641833ba1719ec0bb199c5797ea7d4b
Austin Empaynado <AustinEmpaynado@cmail.carleton.ca>
2021-11-24 00:14:55 -0500

    created bottom nav

--- app/src/main/java/com/example/a3/MainActivity.java
+++ app/src/main/java/com/example/a3/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.a3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
