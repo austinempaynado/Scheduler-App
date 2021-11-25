42fb4c6a4ea31d9fa167fb8c9d044345272718ab
Austin Empaynado <AustinEmpaynado@cmail.carleton.ca>
2021-11-24 22:37:43 -0500

    added time and date pickers

--- app/src/main/java/com/example/a3/add/AddMeetingFragment.java
+++ app/src/main/java/com/example/a3/add/AddMeetingFragment.java
@@ -1,19 +1,24 @@
 package com.example.a3.add;
 
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 
@@ -33,6 +38,7 @@ public class AddMeetingFragment extends Fragment {
 
     final Calendar calendar = Calendar.getInstance();
     private boolean isStartDate = false;
+    private boolean isStartTime = false;
 
 
     public static AddMeetingFragment newInstance() {
@@ -49,38 +55,42 @@ public class AddMeetingFragment extends Fragment {
         binding = AddMeetingFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        DatePickerDialog.OnDateSetListener newDate = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                calendar.set(Calendar.YEAR, i);
-                calendar.set(Calendar.MONTH, i1);
-                calendar.set(Calendar.DAY_OF_MONTH, i2);
 
-                String myFormat = "MM/dd/yyyy";
-                SimpleDateFormat dateF = new SimpleDateFormat(myFormat, Locale.CANADA);
 
-                if(isStartDate){
-                    binding.dateEditText1.setText(dateF.format(calendar.getTime()));
-                }else{
-                    binding.dateEditText2.setText(dateF.format(calendar.getTime()));
-                }
-            }
-        };
-
-        //functions
+        //Date picker
         binding.dateEditText1.setOnClickListener( new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
                 isStartDate = true;
-                setDate(newDate);
+                setDate();
             }
         });
 
         binding.dateEditText2.setOnClickListener( new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
                 isStartDate = false;
-                setDate(newDate);
+                setDate();
+            }
+        });
+
+
+        //Time picker
+        binding.timeEditText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isStartTime = true;
+                setTime();
+            }
+        });
+
+        binding.timeEditText2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isStartTime = false;
+                setTime();
             }
         });
 
@@ -100,12 +110,50 @@ public class AddMeetingFragment extends Fragment {
         return root;
     }
 
-    public void setDate(DatePickerDialog.OnDateSetListener newDate){
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void setDate(){
         new DatePickerDialog(getActivity(),
-                newDate,
+                new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR, i);
+                calendar.set(Calendar.MONTH, i1);
+                calendar.set(Calendar.DAY_OF_MONTH, i2);
+
+                String myFormat = "MM/dd/yyyy";
+                SimpleDateFormat dateF = new SimpleDateFormat(myFormat, Locale.CANADA);
+
+                if(isStartDate){
+                    binding.dateEditText1.setText(dateF.format(calendar.getTime()));
+                    binding.dateEditText2.setText(dateF.format(calendar.getTime()));
+                }else{
+                    binding.dateEditText2.setText(dateF.format(calendar.getTime()));
+                }
+            }
+        },
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void setTime(){
+        Calendar currentTime = Calendar.getInstance();
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = currentTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePicker;
+        timePicker = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                if(isStartTime){
+                    binding.timeEditText1.setText(i + ":" + i1);
+                }else{
+                    binding.timeEditText2.setText(i +":" + i1);
+                }
+            }
+        }, hour, minute, true);
+        timePicker.setTitle("Select Time");
+        timePicker.show();
     }
 
 
